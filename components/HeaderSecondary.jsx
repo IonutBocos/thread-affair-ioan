7de0c2be1494654c85a0b2c9df0f0da2336ce166
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-06-21 14:25:40 +0300

    Color header

--- components/HeaderSecondary.jsx
+++ components/HeaderSecondary.jsx
@@ -10,10 +10,13 @@ export default function HeaderSecondary() {
       <div className="header-support d-none d-lg-block">
         <ul>
           <li>
+            <i class="fa-solid fa-phone"></i>
+          </li>
+
+          <li>
             <a href="tel:1-877-666-1840" title="Call us">
               Toll Free 1-877-666-1840
             </a>
-            I
           </li>
 
           <li>
@@ -45,6 +48,7 @@ export default function HeaderSecondary() {
           <li>
             <a href="https://www.google.ro/?hl=ro" title="Cart">
               <i className="fa-solid fa-cart-shopping"></i>
+              <span class="qty">3</span>
             </a>
           </li>
 
