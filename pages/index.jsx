160917178a9ff4c4c6bb5e3a01f5ce4b5e610ca8
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-06-28 13:49:47 +0300

    homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import HomePayment from '../components/homepage/HomePagyment';
+import ContentBanner from '../components/homepage/ContentBanner';
+import HomePayment from '../components/homepage/HomePayment';
+import HomeSocial from '../components/homepage/HomeSocial';
+import HomeNewsletter from '../components/homepage/HomeNewsletter';
 
 export default function Home() {
   return (
@@ -13,7 +16,17 @@ export default function Home() {
       <Header></Header>
 
       <main className="content">
-        <HomePayment></HomePayment>
+        <header className="content-header mb-5">
+          <ContentBanner></ContentBanner>
+        </header>
+
+        <section className="content-main">
+          <HomeNewsletter></HomeNewsletter>
+
+          <HomeSocial></HomeSocial>
+
+          <HomePayment></HomePayment>
+        </section>
       </main>
 
       <Footer></Footer>
